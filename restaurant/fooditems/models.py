ec859e736a5537e9fa7857fbdeb9a1cd5b9f13ea
Chenchaiah.malli <chenchaiahmalli1234@gmail.com>
2024-09-03 17:37:24 +0530

    version 2

--- restaurant/fooditems/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class FoodItems(models.Model):
-    name = models.CharField(max_length=50)
-    items= models.IntegerField()
-    foodtype = models.CharField(max_length=100)
-    price = models.IntegerField()
-# Create your models here.
